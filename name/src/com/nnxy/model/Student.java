5bbbbcbaf165b1dbfafbe79be5d285a7b54ef020
weiyongquan123 <979808843@qq.com>
2019-05-16 16:56:00 +0800

    提交到本地仓库

---
+++ name/src/com/nnxy/model/Student.java
@@ -0,0 +1,19 @@
+package com.nnxy.model;
+
+public class Student {
+   private int id;
+   private String username;
+   private String password;
+   private int sex;
+   private String love;
+   public Student(int id,String username,String password,int sex,String love) {
+	   super();
+	   this.id=id;
+	   this.username=username;
+	   this.password=password;
+	   this.sex=sex;
+	   this.love =love;
+   }
+
+   
+}
