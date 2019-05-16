4d1c885c648529619dbe30600659cd0c8b61d82e
weiyongquan123 <49516034+weiyongquan123@users.noreply.github.com>
2019-05-16 17:36:21 +0800

    Update Student.java

--- name/src/com/nnxy/model/Student.java
+++ name/src/com/nnxy/model/Student.java
@@ -1,9 +1,9 @@
 package com.nnxy.model;
 
 public class Student {
-   private int id;
-   private String username;
-   private String password;
+   private int id;//唯一标识ID
+   private String username;//用户名
+   private String password;//密码
    private int sex;
    private String love;
    public Student(int id,String username,String password,int sex,String love) {
